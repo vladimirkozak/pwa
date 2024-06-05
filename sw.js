b38474ff7e9e123ebf8024d0f91462cc99c3dda8
vladimir kozak <v.kozak@sotbit.ru>
2024-06-05 12:53:30 +0300

    feat: add fetch

--- sw.js
+++ sw.js
@@ -7,7 +7,24 @@ const assets = [
 ];
 
 self.addEventListener("install", event => {
-  caches.open("assets").then(cache => {
-    cache.addAll(assets);
-  });
+  event.waitUntil(
+    caches.open("assets").then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)  // searching in the cache
+      .then( response => {
+        if (response) {
+          // The request is in the cache
+          return response; // cache hit
+        } else {
+          // We need to go to the network
+          return fetch(event.request);  // cache miss
+        }
+      })
+  );
 });
