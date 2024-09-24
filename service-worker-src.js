ee83462412ad79883ba753f3b4de9da4a2d3c66d
vladimir kozak <v.kozak@sotbit.ru>
2024-09-24 16:50:25 +0300

    install lib

---
+++ service-worker-src.js
@@ -0,0 +1,15 @@
+// const assets = [
+//   "/",
+//   "styles.css",
+//   "app.js",
+//   "sw-register.js",
+//   "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"
+// ];
+
+// self.addEventListener("install", event => {
+//   event.waitUntil(
+//     caches.open("assets").then(cache => {
+//       cache.addAll(assets);
+//     })
+//   );
+// });
